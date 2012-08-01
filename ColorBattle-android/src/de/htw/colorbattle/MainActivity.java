1e7733a58cdf2cf81ec124e1d0553b81535c88bf
Tim Bartsch <tibartschm@googlemail.com>
2012-08-01 19:28:59 +0200

    Enabled Accelerometer and disabled compass in android project.

--- ColorBattle-android/src/de/htw/colorbattle/MainActivity.java
+++ ColorBattle-android/src/de/htw/colorbattle/MainActivity.java
@@ -11,7 +11,9 @@ public class MainActivity extends AndroidApplication {
         super.onCreate(savedInstanceState);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
+        cfg.useGL20 = true;
+        cfg.useAccelerometer = true;
+        cfg.useCompass = false;
         
         initialize(new ColorBattleGame(), cfg);
     }
