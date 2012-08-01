9bf0dea22ad92beed1fd9a684b02685775c4555f
Tim Bartsch <tibartschm@googlemail.com>
2012-08-01 20:44:14 +0200

    Set keep screen on flag.

--- ColorBattle-android/src/de/htw/colorbattle/MainActivity.java
+++ ColorBattle-android/src/de/htw/colorbattle/MainActivity.java
@@ -1,6 +1,7 @@
 package de.htw.colorbattle;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -9,6 +10,8 @@ public class MainActivity extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.useGL20 = true;
