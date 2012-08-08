78302a07f94fb89f3fa2c0ce91bf5cc1febb8a52
Christian Rech <rech.christian@googlemail.com>
2012-08-08 13:57:11 +0200

    Extended NetwerkService with methods for receiving and sending an object

--- ColorBattle/src/de/htw/colorbattle/network/NetworkService.java
+++ ColorBattle/src/de/htw/colorbattle/network/NetworkService.java
@@ -1,6 +1,7 @@
 package de.htw.colorbattle.network;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
@@ -14,14 +15,54 @@ public class NetworkService {
 	private MulticastSocket mcSocket;
 	private int mcPort;
 	
+	private static final int MAX_UDP_DATAGRAM_LEN = 1024;
+	
 	public NetworkService(String mcAddress, int mcPort) throws NetworkException{
         try {
-			mcSocket = new MulticastSocket(mcPort);
-			mcGroup = InetAddress.getByName(mcAddress);
+        	this.mcPort = mcPort;
+			this.mcSocket = new MulticastSocket(mcPort);
+			this.mcGroup = InetAddress.getByName(mcAddress);
+			
+	        mcSocket.joinGroup(mcGroup);
+	      //socket.setSoTimeout(1000); //throws timeout exception
+	        executeService();
 		} catch (Exception e) {
 			Gdx.app.error("NetworkService", "Can't create NetworkService", e);
 			throw new NetworkException("Can't create NetworkService: \n" + e.toString());
 		} 
+	}
+	
+	private void executeService() {
+		new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					receive(); 
+				}
+			} 
+		}).start(); 
+	}
+	
+	private void receive(){
+		byte[] buffer = new byte[MAX_UDP_DATAGRAM_LEN];
+		DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
+		try {
+			mcSocket.receive(receivedPacket);
+			Object obj = SerializeUtils.deserializeObject(receivedPacket.getData());
+			//TODO: handle obj
+		} catch (IOException e) {
+			Gdx.app.error("NetworkService", "Can't receive package", e);
+		}
+	}
+	
+	public void send(Object obj) throws NetworkException{
+		try {
+			byte[] msg = SerializeUtils.serializeObject(obj);
+			DatagramPacket data = new DatagramPacket(msg, 0, msg.length, mcGroup, mcPort);
+			mcSocket.send(data);
+		} catch (Exception e) {
+			Gdx.app.error("NetworkService", "Can't send message", e);
+			throw new NetworkException("Can't send message: \n" + e.toString());
+		}
 	}
 
 }
