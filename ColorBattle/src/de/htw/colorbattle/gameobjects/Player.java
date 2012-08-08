13fc16ab56ba307ba265e7de55189c2b5f7a7097
Christian Rech <rech.christian@googlemail.com>
2012-08-08 17:08:36 +0200

    Add toString methode to Player class

--- ColorBattle/src/de/htw/colorbattle/gameobjects/Player.java
+++ ColorBattle/src/de/htw/colorbattle/gameobjects/Player.java
@@ -1,10 +1,13 @@
 package de.htw.colorbattle.gameobjects;
 
+import java.io.Serializable;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 
 
-public class Player {
+public class Player implements Serializable{
+	public int id; //TODO set ID
 	public float x;
 	public float y;
 	public float radius;
@@ -32,5 +35,14 @@ public class Player {
 	public void move(){
 		this.x += this.speed * this.directionX * Gdx.graphics.getDeltaTime();
 		this.y -= this.speed * this.directionY * Gdx.graphics.getDeltaTime();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("Player").append(id)
+								.append("\n Position X: ").append(this.x)
+								.append("\n Position Y: ").append(this.y);
+		
+		return sb.toString();
 	}
 }
