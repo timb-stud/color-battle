a8dd42bd3cc4039d1da0bc91daf52bbbfb5c1b6c
YKlos <yassir.klos@googlemail.com>
2012-08-10 16:04:14 +0200

    Added MainMenu Buttons funcionality

--- ColorBattle/src/de/htw/colorbattle/MainMenuScreen.java
+++ ColorBattle/src/de/htw/colorbattle/MainMenuScreen.java
@@ -1,38 +1,44 @@
 package de.htw.colorbattle;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MainMenuScreen implements Screen {
+public class MainMenuScreen implements Screen, InputProcessor {
 	private ColorBattleGame game;
     private SpriteBatch spriteBatch;
-    private Texture splsh;
+    private Sprite newGameSprite;
+    private Sprite exitGameSprite;
     
     /**
-     * Constructor for the splash screen
-     * @param g Game which called this splash screen.
+     * Constructor for the menue screen
+     * @param ColorBattleGame game which called this menue screen.
      */
 	public MainMenuScreen(ColorBattleGame game) {
 		this.game = game;
 		spriteBatch = new SpriteBatch();
-        splsh = new Texture(Gdx.files.internal("menue.png"));
+		
+		newGameSprite = new Sprite(new Texture(Gdx.files.internal("new_game.png")));
+		newGameSprite.setPosition((Gdx.graphics.getWidth() - newGameSprite.getWidth()) / 2.0f, Gdx.graphics.getHeight() - 200.0f);
+		
+		exitGameSprite = new Sprite(new Texture(Gdx.files.internal("exit_game.png")));
+		exitGameSprite.setPosition((Gdx.graphics.getWidth() - exitGameSprite.getWidth()) / 2.0f, Gdx.graphics.getHeight() - 400.0f);
+		
+        Gdx.input.setInputProcessor(this);
 	}
 	
 	@Override
 	public void render(float delta) {
-		// update and draw stuff
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        spriteBatch.begin();
-        spriteBatch.draw(splsh, 0, 0);
-        spriteBatch.end();
-        
-        if (Gdx.input.justTouched()) {// use your own criterion here
-        	Gdx.app.log("MainMenuScreen", "Just Touched");
-            game.setScreen(game.gameScreen);
-        }
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		spriteBatch.begin();
+		newGameSprite.draw(spriteBatch);
+		exitGameSprite.draw(spriteBatch);
+		spriteBatch.end();
 	}
 
 	@Override
@@ -60,5 +66,51 @@ public class MainMenuScreen implements Screen {
 	@Override
 	public void dispose() {
 		// never called automatically!!!
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int x, int y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		if (newGameSprite.getBoundingRectangle().contains(x, Gdx.graphics.getHeight() - y)) {
+			game.setScreen(game.gameScreen);
+		}
+		if (exitGameSprite.getBoundingRectangle().contains(x, Gdx.graphics.getHeight() - y)) {
+			Gdx.app.exit();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int x, int y, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean touchMoved(int x, int y) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
