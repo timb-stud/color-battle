b3ec43587384398bf6cb985b9c779af9ad5b8614
Christian Rech <rech.christian@googlemail.com>
2012-08-22 00:42:30 +0200

    setLoopBackMode to Disable for Multicast messages

--- ColorBattle/src/de/htw/colorbattle/network/NetworkService.java
+++ ColorBattle/src/de/htw/colorbattle/network/NetworkService.java
@@ -21,7 +21,6 @@ public class NetworkService extends Observable {
 	private InetAddress mcGroup;
 	private MulticastSocket mcSocket;
 	private int mcPort;
-	private String ownIpAddress;
 	
 	private static final int MAX_UDP_DATAGRAM_LEN = 1024;
 	
@@ -31,8 +30,8 @@ public class NetworkService extends Observable {
 			this.mcSocket = new MulticastSocket(mcPort);
 			this.mcGroup = InetAddress.getByName(mcAddress);
 	        mcSocket.joinGroup(mcGroup);
+	        mcSocket.setLoopbackMode(true);
 	      //socket.setSoTimeout(1000); //throws timeout exception
-	        this.ownIpAddress = getLocalIpAddress();
 	        executeService();
 		} catch (Exception e) {
 			Gdx.app.error("NetworkService", "Can't create NetworkService", e);
@@ -55,9 +54,6 @@ public class NetworkService extends Observable {
 		DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
 		try {
 			mcSocket.receive(receivedPacket);
-			Gdx.app.debug("IP Address", "own IP: " + ownIpAddress + " received IP: " + receivedPacket.getAddress());
-			if(receivedPacket.getAddress().toString().equals(ownIpAddress))
-				return;
 			Object obj = SerializeUtils.deserializeObject(receivedPacket.getData());
 			//Gdx.app.debug("Receiving", "new package from " + receivedPacket.getAddress());
 		    setChanged();
@@ -76,26 +72,6 @@ public class NetworkService extends Observable {
 			Gdx.app.error("NetworkService", "Can't send message", e);
 			throw new NetworkException("Can't send message: \n" + e.toString());
 		}
-	}
-	
-	private String getLocalIpAddress() {
-	    try {
-	        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
-	            NetworkInterface intf = en.nextElement();
-	            for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
-	                InetAddress inetAddress = enumIpAddr.nextElement();
-	                if (!inetAddress.isLoopbackAddress()) {
-	                    String ip = InetAddress.getByName(inetAddress.toString()).toString();
-	                    return ip;
-	                }
-	            }
-	        }
-	    } catch (SocketException ex) {
-	    	Gdx.app.error("NetworkService", "Can't get own IP", ex);
-	    } catch (UnknownHostException e) {
-	    	Gdx.app.error("NetworkService", "Can't get own IP", e);
-		}
-	    return null;
 	}
 
 }
