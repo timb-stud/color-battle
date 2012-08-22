51d0df5a75d058416aad46a85f2b3b107a57292d
Christian Rech <rech.christian@googlemail.com>
2012-08-22 17:05:27 +0200

    set default display width and hight

--- ColorBattle/src/de/htw/colorbattle/config/BattleColorConfig.java
+++ ColorBattle/src/de/htw/colorbattle/config/BattleColorConfig.java
@@ -5,10 +5,11 @@ public class BattleColorConfig {
 	public String multicastAddress;
 	public int multicastPort;
 	public boolean playSound;
+	
 	//only default value, values updated in main activity
 	public int multigamePlayerCount = 1; 
 	public float networkPxlUpdateIntervall = 1;
 	public int gameTime = 60;
-	public int width;
-	public int height;
+	public int width = 800;
+	public int height = 480;
 }
