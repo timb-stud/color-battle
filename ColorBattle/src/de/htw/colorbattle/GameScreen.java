f6d077d4ec872e3658a0211a8c38f6c7aca204fc
Andreas W <mapsmail@gmx.de>
2012-09-17 09:57:21 +0200

    neu geordnet, kommentare und todos

--- ColorBattle/src/de/htw/colorbattle/GameScreen.java
+++ ColorBattle/src/de/htw/colorbattle/GameScreen.java
@@ -46,129 +46,115 @@ public class GameScreen implements Screen {
 	private boolean scoreComputed = false;
 	public Texture endTexture;
 	private LinkedList<Player> playerList = new LinkedList<Player>();
-	
+
 	public GameScreen(ColorBattleGame game) throws NetworkException {
 		this.game = game;
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
-		
-		//Spielfeld
+
+		// Spielfeld
 		width = (int) this.game.camera.viewportWidth;
 		height = (int) this.game.camera.viewportHeight;
 		batch = new SpriteBatch();
 		colorFrameBuffer = new FrameBuffer(Format.RGBA8888, width, height,
 				false);
 		gameBorder = new GameBorder(width, height);
-		countDown = new CountDown(Color.ORANGE, 480);		
-		
-		//Player Allgemein
+		countDown = new CountDown(Color.ORANGE, 480);
+
+		// Player Allgemein
 		flipper = new TextureRegion();
 		playerTexture = new Texture(Gdx.files.internal("player.png"));
 		int playerWidth = playerTexture.getWidth();
 		int playerHeight = playerTexture.getHeight();
 		playerMap = new HashMap<Integer, Player>();
-		
+
 		// spezielle Player
 		player = new Player(Color.GREEN, playerWidth / 2);
 		player.setColorInt(Color.GREEN);
 		player.x = width / 2 - playerWidth / 2;
 		player.y = height / 2 - playerHeight / 2;
-		
-		playerSimulation = new PlayerSimulation(player);
-		
-		otherPlayer = new Player(Color.RED, playerWidth / 2);
-		otherPlayer.setColorInt(Color.RED);			
 
-		if (game.bcConfig.gameMode == GameMode.WIFI && game.bcConfig.isWifiConnected) {
-			this.netSvc = NetworkService.getInstance(game.bcConfig.multicastAddress, game.bcConfig.multicastPort);
+		playerSimulation = new PlayerSimulation(player);
+
+		otherPlayer = new Player(Color.RED, playerWidth / 2);
+		otherPlayer.setColorInt(Color.RED);
+
+		if (game.bcConfig.gameMode == GameMode.WIFI
+				&& game.bcConfig.isWifiConnected) {
+			this.netSvc = NetworkService
+					.getInstance(game.bcConfig.multicastAddress,
+							game.bcConfig.multicastPort);
 		}
-		
+
 	}
 
-	public void swapPlayers(){
+	public void swapPlayers() {
 		Player buffer = player;
 		player = otherPlayer;
 		otherPlayer = buffer;
 	}
 
-	
 	@Override
 	public void render(float delta) {
+		// Screen und Kamera
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-				
 		game.camera.update();
 		batch.setProjectionMatrix(game.camera.combined);
 
-		colorFrameBuffer.begin();
-			batch.begin();
-				batch.draw(player.colorTexture, player.x, player.y);
-//				for (Player p : playerMap.values()){
-//					batch.draw(p.colorTexture, p.x, p.y);
-//				}
-				batch.draw(otherPlayer.colorTexture, otherPlayer.x, otherPlayer.y);
-			batch.end();
-		colorFrameBuffer.end();
-
+		// Player zeichnen // TODO alle Schritte wirklich nötig ?
 		flipper.setRegion(colorFrameBuffer.getColorBufferTexture());
 		flipper.flip(false, true);
-
+		colorFrameBuffer.begin();
 		batch.begin();
-			batch.draw(wallpaper, 0, 0);
-			batch.draw(flipper, 0, 0);
-			batch.draw(playerTexture, player.x, player.y);
-//			for (Player p : playerMap.values()){
-//				batch.draw(playerTexture, p.x, p.y);
-//			}
-			batch.draw(playerTexture, otherPlayer.x, otherPlayer.y);
-			batch.draw(countDown.countDownTexture, countDown.x, countDown.y);
+		batch.draw(player.colorTexture, player.x, player.y);
+		batch.draw(otherPlayer.colorTexture, otherPlayer.x, otherPlayer.y);
+		batch.end();
+		colorFrameBuffer.end();
+		// TODO unterschied colorframebuffer und normaler batch ?
+		batch.begin();
+		batch.draw(wallpaper, 0, 0); // Hintergrund
+		batch.draw(flipper, 0, 0);
+		batch.draw(playerTexture, player.x, player.y);
+		batch.draw(playerTexture, otherPlayer.x, otherPlayer.y);
+		batch.draw(countDown.countDownTexture, countDown.x, countDown.y); // Zeit
 		batch.end();
 
+		// Player movement // TODO 3 Bewegungen ?
 		Accelerometer.updateDirection(player.direction);
-		if (Gdx.input.isKeyPressed(Keys.UP))
-			player.y += player.speed * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Keys.DOWN))
-			player.y -= player.speed * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Keys.LEFT))
-			player.x -= player.speed * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Keys.RIGHT))
-			player.x += player.speed * Gdx.graphics.getDeltaTime();
-
+		// checkDesktopControl(); // not supported atm
 		player.move();
 		gameBorder.handelCollision(player);
 		playerSimulation.move();
 		gameBorder.handelCollision(playerSimulation);
-//		for (Player p : playerMap.values()){
-//			p.move();
-//			gameBorder.handelCollision(p);
-//		}
 		otherPlayer.move();
 		gameBorder.handelCollision(otherPlayer);
 
 		
-		if (!gameEnd){
-			gameEnd = countDown.activateCountDown(endTime, game.bcConfig.gameTime);
-		}
-		if (gameEnd){	
+		// Game End
+		if (!gameEnd) {
+			gameEnd = countDown.activateCountDown(endTime,
+					game.bcConfig.gameTime);
+		}else{
 			enterPlayerList();
 			game.setScreen(game.gameEndScreen);
-			if (scoreComputed){
-				if (endTexture != null){
+			if (scoreComputed) {
+				if (endTexture != null) {
 					batch.begin();
-//					batch.draw(endTexture, 100, 50);
+					// batch.draw(endTexture, 100, 50);
 					batch.end();
 				}
-			}else {
+			} else {
 				this.endTexture = computeScore();
 				scoreComputed = true;
 			}
-		//	playerTexture.dispose();
-		//	player.dispose();
-		//	colorFrameBuffer.dispose();
+			// playerTexture.dispose();
+			// player.dispose();
+			// colorFrameBuffer.dispose();
 		}
 
-		
-		if (netSvc != null || game.bcConfig.gameMode == GameMode.BLUETOOTH){
-			if(playerSimulation.distance(player) > game.bcConfig.networkPxlUpdateIntervall){
+		// NetworkCommunication
+		if (netSvc != null || game.bcConfig.gameMode == GameMode.BLUETOOTH) {
+			if (playerSimulation.distance(player) > game.bcConfig.networkPxlUpdateIntervall) {
 				playerSimulation.update(player);
 				sendPosition();
 			}
@@ -177,7 +163,7 @@ public class GameScreen implements Screen {
 
 	private void sendPosition() {
 		try {
-			if(game.bcConfig.gameMode == GameMode.WIFI)
+			if (game.bcConfig.gameMode == GameMode.WIFI)
 				netSvc.send(playerSimulation);
 			else
 				game.bluetoothActionResolver.send(playerSimulation);
@@ -187,68 +173,27 @@ public class GameScreen implements Screen {
 		}
 	}
 
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void show() {
-		endTime = System.currentTimeMillis() /1000 + game.bcConfig.gameTime;
-		Gdx.app.log("GameScreen", "show();");
-
-		if (game.bcConfig.playSound) {
-			game.playSound();
-		}
-		// called when this screen is set as the screen with game.setScreen();
-		wallpaper = new Texture(Gdx.files.internal("GameScreenWallpaper.png"));
-	}
-
-	@Override
-	public void hide() {
-		// called when current screen changes from this to a different screen
-	}
-
-	@Override
-	public void pause() {
-
-	}
-
-	@Override
-	public void resume() {
-	}
-
-	@Override
-	public void dispose() {
-		// never called automatically!!!
-		playerTexture.dispose();
-		player.dispose();
-		otherPlayer.dispose();
-		colorFrameBuffer.dispose();
-		batch.dispose();
-	}
-
-
 	public Texture computeScore() {
 		LinkedList<Player> playerList = new LinkedList<Player>();
 		playerList.add(player);
-		for (Player p : playerMap.values()){
+		for (Player p : playerMap.values()) {
 			playerList.add(p);
 		}
 
 		GameResult gr = new GameResult(playerList);
-		//System.out.println(gr.getScoredPlayerList().toString());
-		//Gdx.app.debug("Player scores", gr.getScoredPlayerList().toString());
+		// System.out.println(gr.getScoredPlayerList().toString());
+		// Gdx.app.debug("Player scores", gr.getScoredPlayerList().toString());
 		return gr.getScoreScreen(batch);
 	}
-	
-	private void enterPlayerList(){
+
+	private void enterPlayerList() {
 		this.playerList.add(player);
-		for(Player p : playerMap.values()){
+		for (Player p : playerMap.values()) {
 			this.playerList.add(p);
 		}
 	}
-	
-	public LinkedList<Player> getPlayerList(){		
+
+	public LinkedList<Player> getPlayerList() {
 		return playerList;
 	}
 
@@ -266,10 +211,11 @@ public class GameScreen implements Screen {
 
 	public void setPlayerMap(HashMap<Integer, Player> playerMap) {
 		Iterator<Player> i = playerMap.values().iterator();
-		this.otherPlayer.update(i.next()); //TODO only for playing with 2 players
+		this.otherPlayer.update(i.next()); // TODO only for playing with 2
+											// players
 		this.playerMap = playerMap;
 	}
-	
+
 	public Player getPlayer() {
 		return player;
 	}
@@ -277,12 +223,72 @@ public class GameScreen implements Screen {
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
-	
+
 	public Player getOtherPlayer() {
 		return otherPlayer;
 	}
-	
-	public void updateOtherPlayer(PlayerSimulation ps){
+
+	public void updateOtherPlayer(PlayerSimulation ps) {
 		otherPlayer.update(ps);
+	}
+
+	/**
+	 * checks Input-Keys for Desktop Version
+	 */
+	private void checkDesktopControl() {
+		if (Gdx.input.isKeyPressed(Keys.UP))
+			player.y += player.speed * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Keys.DOWN))
+			player.y -= player.speed * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Keys.LEFT))
+			player.x -= player.speed * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Keys.RIGHT))
+			player.x += player.speed * Gdx.graphics.getDeltaTime();
+	}
+
+	// ---------------------- down libgdx Elements ----------------------
+	
+	/**
+	 * Called when this screen becomes the current screen for a Game.
+	 * also einmal beim Spielstart!
+	 */
+	@Override
+	public void show() {
+		Gdx.app.log("GameScreen", "show();");
+		endTime = System.currentTimeMillis() / 1000 + game.bcConfig.gameTime;
+		
+		if (game.bcConfig.playSound) {
+			game.playSound();
+		}
+		// called when this screen is set as the screen with game.setScreen();
+		wallpaper = new Texture(Gdx.files.internal("GameScreenWallpaper.png"));
+		// TODO vllt kann man den Wallpaper direkt zeichnen auf das Element das nicht gelöscht wird , und nicht extra nochmal im render
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void hide() {
+		// called when current screen changes from this to a different screen
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+		// never called automatically!!!
+		playerTexture.dispose();
+		player.dispose();
+		otherPlayer.dispose();
+		colorFrameBuffer.dispose();
+		batch.dispose();
 	}
 }
