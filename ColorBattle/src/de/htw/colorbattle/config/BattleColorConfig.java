cdfa651ec76ab6533cc7210d071fd23a3d3dc2e6
Andreas W <mapsmail@gmx.de>
2012-09-20 17:03:05 +0200

    man wieder die zeit verstellt ^^

--- ColorBattle/src/de/htw/colorbattle/config/BattleColorConfig.java
+++ ColorBattle/src/de/htw/colorbattle/config/BattleColorConfig.java
@@ -4,7 +4,7 @@ package de.htw.colorbattle.config;
  * Für eine feste Konfiguration
  */
 public class BattleColorConfig {
-	public static final int GAME_TIME = 10;
+	public static final int GAME_TIME = 60;
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 480;
 	
