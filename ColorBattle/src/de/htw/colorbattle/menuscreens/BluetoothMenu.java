e67786300ec0a154a850d8cea5966edf97b72973
Andreas W <mapsmail@gmx.de>
2012-09-22 00:01:43 +0200

    unnötige imports entfernt

--- ColorBattle/src/de/htw/colorbattle/menuscreens/BluetoothMenu.java
+++ ColorBattle/src/de/htw/colorbattle/menuscreens/BluetoothMenu.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import de.htw.colorbattle.ColorBattleGame;
 import de.htw.colorbattle.config.BattleColorConfig;
-import de.htw.colorbattle.exception.NetworkException;
 import de.htw.colorbattle.multiplayer.MultigameLogic;
 
 /**
